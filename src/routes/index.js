14af564747b86a2d45b88cf552f2eed997260864
Vovk Vladyslav <VKolo.bokV@gmail.com>
2016-07-29 00:11:15 +0300

    User routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,50 @@ module.exports = function (app) {
     app.get(API_PREFIX + '/version', function (request, response, next) {
         response.send(VERSION);
     });
+
+    // User
+    app.get(API_PREFIX + '/user', function (request, response, next) {
+        User.find({}).then(function (allUsers) {
+                response.send(allUsers);
+            },
+            function () {
+                next(error.getHttpError(400, 'Can not get users'));
+            });
+    });
+
+    app.get(API_PREFIX + '/user:id', function (request, response, next) {
+        User.findById(request.params.id).then(function (foundUser) {
+                response.send(foundUser);
+            },
+            function () {
+                next(error.getHttpError(400, 'Can not find user with such id'));
+            });
+    });
+
+    app.post(API_Prefix + '/user', function (request, response, next) {
+        User.create(request.body).then(function (user) {
+                response.send({ _id: user._id });
+            },
+            function () {
+                next(error.getHttpError(400, 'Can not create user'));
+            });
+    });
+
+    app.put(API_PREFIX + '/user"id', function (request, response, next) {
+        User.update({ _id: request.params.id }, request.body).then(function () {
+                response.send();
+            },
+            function () {
+                next(error.getHttpError(400, 'Could not update user'));
+            });
+    });
+
+    app.delete(API_PREFIX + '/user:id', function (request, response, next) {
+        User.remove({ _id: request.params.id }).then(function () {
+                response.send();
+            },
+            function () {
+                next(error.getHttpError(400, 'Could not remove user with such id'));
+            });
+    });
 };
